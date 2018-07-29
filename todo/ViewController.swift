d1a26fbd9232ad8524c2a83696d62d10ba961c78
森川彩音 <aya@morikawaayanenoMacBook-Pro.local>
2018-07-29 11:18:20 +0900

    最新

--- todo/ViewController.swift
+++ todo/ViewController.swift
@@ -9,49 +9,68 @@
 import UIKit
 import RealmSwift
 
-class ViewController: UITableViewController {
+class ViewController: UIViewController,UITableViewDataSource{
     
     // 追加
     var todoItem: Results<Birthday>!
     
+    @IBOutlet weak var mainTable: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        mainTable.dataSource = self
         
         // 追加
         // 永続化されているデータを取りだす
         do{
             let realm = try Realm()
             todoItem = realm.objects(Birthday.self)
-            tableView.reloadData()
+            mainTable.reloadData()
         }catch{
             
         }
         
     }
-
-        override func didReceiveMemoryWarning() {
-            super.didReceiveMemoryWarning()
-        }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int)->Int {
+//        return todoItem.count
+//    }
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return todoItem.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int)->Int {
-            return todoItem.count
-        }
-
-        func tableView(tableView: UITableView, cellForRowAtIndexPath indexpath: NSIndexPath)->UITableViewCell {
-            
-            let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
-            let object = todoItem[indexpath.row]
-            
-            //cellのtextLabelのtextにobjectのtitleプロパティを代入
-            cell.textLabel?.text = object.name
-            
-            return cell
-        }
+//                let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+//                let object = todoItem[indexpath.row]
+        
+                //cellのtextLabelのtextにobjectのtitleプロパティを代入
+//                cell.textLabel?.text = object.name
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: <#T##IndexPath#>)
+        cell.textLabel?.text = "sample"
+        
+                return cell
+    }
+    
+//    func tableView(tableView: UITableView, cellForRowAtIndexPath indexpath: IndexPath)->UITableViewCell {
+//        
+//        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "Cell")
+//        let object = todoItem[indexpath.row]
+//        
+//        //cellのtextLabelのtextにobjectのtitleプロパティを代入
+//        cell.textLabel?.text = object.name
+//        
+//        return cell
+//    }
     
     // 追加 画面が表示される際などにtableViewのデータを再読み込みする
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        tableView.reloadData()
+        mainTable.reloadData()
         
     }
 }
